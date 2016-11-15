aef62d151a2fa2b005f5ef818dff90bb700cc1a3
DavidThompsonZA <david@parcelninja.com>
2016-11-15 18:54:52 +0200

    commit

--- game.js
+++ game.js
@@ -15,11 +15,197 @@ $('.btnNewGame').click(function() {
 	newGame();
 });
 $('.btnNewPlayer').click(function() {
-	resetGame();
+	resetApp();
 });
 $('.btnCorrectionContinue').click(function() {
 	clearCorrectionFlag();
 });
+
+function postScore(gameData) {
+	$.ajax({
+	  type: "POST",
+	  url: "https://requestb.in/1au79rm1",
+	  data: gameData
+	});
+}
+
+function setPlayer(name) {
+	if (name.length > 0) {
+    	gameData.playerName = name;
+    	drawViews();
+    }
+}
+
+function restartGame() {
+	gameData.gameStep = 0;
+	gameData.tryCount = 0;
+	startGame(gameData.gameNo);
+}
+
+function newGame() {
+	gameData.gameNo = undefined;
+	gameData.gameStep = 0;
+	gameData.tryCount = 0;
+	drawViews();
+}
+
+function clearCorrectionFlag() {
+	gameData.correctionFlag = false;
+	drawViews();
+}
+
+function checkWord(word) {
+	if (word.length < 1) {
+		alertify.error("Please enter the answer first");
+		return;
+	}
+	gameData.tryCount++;
+
+	var isCorrect = false;
+	var rslt = null;
+	var answers = gameData.source.words[gameData.gameStep].answers;
+	$.each(answers, function(index, value) { 
+		if (rslt == null && value.toLowerCase() === word.toLowerCase()) {
+			rslt = index;
+			isCorrect = true;
+		}
+	});
+	
+	gameData.source.words[gameData.gameStep].tries = gameData.tryCount;
+	if (isCorrect) {
+		var msg = "<img src='https://media.giphy.com/media/3oz8xPjPPvOwrGjip2/giphy.gif' width='200px'><h3>Correct</h3><p>Well done !</p>";
+		alertify.delay(5000).success(msg);
+		gameData.source.words[gameData.gameStep].score = 1;
+		gameData.source.words[gameData.gameStep].given = word;
+		gameData.gameStep++;
+		gameData.tryCount = 0;
+	} else { 							//not correct 
+		if (gameData.source.words[gameData.gameStep].wrongGiven == undefined)
+			gameData.source.words[gameData.gameStep].wrongGiven = [];
+		gameData.source.words[gameData.gameStep].wrongGiven.push(word);
+		if (gameData.tryCount >= 3){
+			var msg = "<img src='https://media.giphy.com/media/l3vQYxsyauUub9oJi/giphy.gif' width='200px'><h3>Out of Luck</h3><p>Review the correct answer</p>";
+			alertify.delay(8000).error(msg);
+			gameData.gameStep++;
+			gameData.tryCount = 0;
+			gameData.correctionFlag = true;
+		} else {
+			var msg = "<img src='https://media.giphy.com/media/l0HlIkUigWdQLaWju/giphy.gif' width='200px'><h3>Wrong</h3><p>" + word + " is wrong, try again</p>";
+			alertify.delay(5000).error(msg);
+		}
+	}
+
+	drawViews();
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function enrichWordListWithHints(words) {
+	var hintDifficulty = 3; //1 = minimum/no hints. Higher numbers = more difficult
+	var newWords = [];
+
+	$.each(words, function(index, value) { 
+		var answer = "";
+		if (value.answers != undefined && value.answers.length > 0) {
+			answer = value.answers[0];
+		}
+
+		var hint = "No hint";
+		if (answer.length > 2) {
+			hint = "";
+			var letterCount = answer.length;
+			var first = 0;
+			var last = letterCount - 1;
+			//hint = answer[first] + " " + Array(letterCount - 1).join("_ ") + answer[last];
+			var lastReveal = 0;
+			for (var i = 0; i < letterCount; i++) {
+				if (i == 0 || i >= (letterCount - 1)) {
+					hint += answer[i];
+				} else {
+					var max = 1;
+					if (lastReveal < hintDifficulty) {
+						max = hintDifficulty - lastReveal;
+					}
+					if (max == 1 || getRandomInt(1, max) == 1) {
+						hint += answer[i];
+						lastReveal = 0;
+					} else {
+						hint += "_";
+						lastReveal++;
+					}
+				}
+			}
+		}
+		value.hint = hint;
+		newWords.push(value);
+	});
+	return newWords;
+}
+
+function randomizeAndReduceGameWordsList(words, wordCount) {
+	if (wordCount == undefined || wordCount < 0)
+		wordCount = 10;
+
+	var newWords = [];
+	$.each(words, function(index, value) { 
+		value.randomId = Math.random();;
+		newWords.push(value);
+	});
+	
+	newWords.sort(function(a,b) {
+		return ((a.randomId > b.randomId) ? 1 : -1);
+	});
+
+	wordCount = ((newWords.length < wordCount) ? newWords.length : wordCount);
+	return newWords.slice(0, wordCount);
+}
+
+
+function getGameData(gameNo, setGameSourceCallback, failReason) {
+	var url = "";
+	var result = "";
+
+	url = gAppConfig.games[gameNo - 1].data;
+	
+	if (url.length > 0) {
+		alertify.success("Pulling game from internet...please wait...");
+		$.ajax({
+			type: 'GET',
+		    url: url,
+		    timeout: 10000,
+		    success: function( response ) {
+		    	setGameSourceCallback(true, response);
+		        //result = response; //jQuery.parseJSON( response );;
+		    },
+		    error: function(xhr, textStatus, errorThrown){
+		       setGameSourceCallback(false, undefined, textStatus + " - " + errorThrown);
+		    }
+		});
+	} else {
+		setGameSourceCallback(false, undefined, "Invalid game option selected");
+	}
+}
+
+function startGame(gameNo) {
+	//load a game
+
+	getGameData(gameNo, function(success, source, reason) {
+		if (success) {
+			newGameSource = source;
+			newGameSource.words = randomizeAndReduceGameWordsList(newGameSource.words, source.wordCount);
+			newGameSource.words = enrichWordListWithHints(newGameSource.words);
+			gameData.source = newGameSource;
+			gameData.gameNo = gameNo;
+			drawViews();
+		} else {
+			alertify.error("Could not get game - " + reason);
+		}
+	});
+	
+}
 
 function drawViews(){
 	$(".loadingView").hide();
@@ -74,6 +260,7 @@ function drawViews(){
 					$("#sourceWordLabel").text(gameData.source.words[gameData.gameStep].word);
 					$("#retriesLeft").text(3 - gameData.tryCount);
 					$('#inputWord').val("");
+					$("#lblHintWord").text(gameData.source.words[gameData.gameStep].hint);
 					$(".playView").show();
 					$('#inputWord').focus();
 				}
@@ -82,70 +269,7 @@ function drawViews(){
 	}
 }
 
-function postScore(gameData) {
-	$.ajax({
-	  type: "POST",
-	  url: "https://requestb.in/1au79rm1",
-	  data: gameData
-	});
-}
-
-function setPlayer(name) {
-	if (name.length > 0) {
-    	gameData.playerName = name;
-    	drawViews();
-    }
-}
-
-function startGame(gameNo) {
-	//load a game
-
-	getGameData(gameNo, function(success, source, reason) {
-		if (success) {
-			newGameSource = source;
-			newGameSource.words = randomizeAndReduceGameWordsList(newGameSource.words, game.wordCount);
-			gameData.source = newGameSource;
-			gameData.gameNo = gameNo;
-			drawViews();
-		} else {
-			alertify.error("Could not get game - " + reason);
-		}
-	});
-	
-}
-
-function randomizeAndReduceGameWordsList(words, wordCount) {
-	if (wordCount == undefined || wordCount < 0)
-		wordCount = 10;
-
-	var newWords = [];
-	$.each(words, function(index, value) { 
-		value.randomId = Math.random();;
-		newWords.push(value);
-	});
-	
-	newWords.sort(function(a,b) {
-		return ((a.randomId > b.randomId) ? 1 : -1);
-	});
-
-	wordCount = ((newWords.length < wordCount) ? newWords.length : wordCount);
-	return newWords.slice(0, wordCount);
-}
-
-function restartGame() {
-	gameData.gameStep = 0;
-	gameData.tryCount = 0;
-	startGame(gameData.gameNo);
-}
-
-function newGame() {
-	gameData.gameNo = undefined;
-	gameData.gameStep = 0;
-	gameData.tryCount = 0;
-	drawViews();
-}
-
-function resetGame() {
+function resetApp() {
 	gameData = {
 		playerName : "",
 		gameNo : 0,
@@ -156,54 +280,47 @@ function resetGame() {
 	drawViews();
 }
 
-function clearCorrectionFlag() {
-	gameData.correctionFlag = false;
-	drawViews();
-}
+function getAppConfig(setupAppConfigCallback) {
+	console.log("Getting app configuration from internet");
+	var url = "https://api.myjson.com/bins/t27k";
 
-function checkWord(word) {
-	if (word.length < 1) {
-		alertify.error("Please enter the answer first");
-		return;
-	}
-	gameData.tryCount++;
-
-	var isCorrect = false;
-	var rslt = null;
-	var answers = gameData.source.words[gameData.gameStep].answers;
-	$.each(answers, function(index, value) { 
-		if (rslt == null && value.toLowerCase() === word.toLowerCase()) {
-			rslt = index;
-			isCorrect = true;
-		}
+	$.ajax({
+		type: 'GET',
+	    url: url,
+	    timeout: 10000,
+	    success: function( response ) {
+	    	console.log("Got app configuration from internet");
+			setupAppConfigCallback(response);
+	    },
+	    error: function(xhr, textStatus, errorThrown){
+	    	console.log("Failed to get app config from internet - " + textStatus + ", error is: " + errorThrown);
+	        alertify.error("Could not get app configurations - " + errorThrown);
+	    }
 	});
-	
-	if (isCorrect) {
-		var msg = "<img src='https://media.giphy.com/media/3oz8xPjPPvOwrGjip2/giphy.gif' width='200px'><h3>Correct</h3><p>Well done !</p>";
-		alertify.delay(5000).success(msg);
-		gameData.source.words[gameData.gameStep].score = 1;
-		gameData.source.words[gameData.gameStep].given = word;
-		gameData.gameStep++;
-		gameData.tryCount = 0;
-	} else { 							//not correct 
-
-		gameData.source.words[gameData.gameStep].wrongGiven.push(word);
-		if (gameData.tryCount >= 3){
-			var msg = "<img src='https://media.giphy.com/media/l3vQYxsyauUub9oJi/giphy.gif' width='200px'><h3>Out of Luck</h3><p>Review the correct answer</p>";
-			alertify.delay(8000).error(msg);
-			gameData.gameStep++;
-			gameData.tryCount = 0;
-			gameData.correctionFlag = true;
-		} else {
-			var msg = "<img src='https://media.giphy.com/media/l0HlIkUigWdQLaWju/giphy.gif' width='200px'><h3>Wrong</h3><p>" + word + " is wrong, try again</p>";
-			alertify.delay(5000).error(msg);
-		}
-	}
-
-	drawViews();
 }
 
-function buildNumberedHtmlList(arr) {
+function configure() {
+	console.log("Loading app configuration");
+	getAppConfig(function(configs) {
+		gAppConfig = configs;
+		resetApp(gAppConfig);
+	});
+}
+
+
+var gAppConfig;
+var gameData;
+var newGameSource;
+
+
+$(window).on("load", function() {
+	console.log("Window loaded");
+	configure();
+});
+
+// HTML builder functions
+
+function buildNumberedHtmlList() {
 	var html = "";
 	var counter = 1;
 	$.each(arr, function(index, value) { 
@@ -214,7 +331,7 @@ function buildNumberedHtmlList(arr) {
 
 function buildGameMenuHtml() {
 	var result = "";
-	$.each(game.games, function(index, value) { 
+	$.each(gAppConfig.games, function(index, value) { 
 		result += "<a class=\"list-group-item menuOption\" onclick=\"startGame(" + (index + 1) + ");\"><span class=\"glyphicon glyphicon-play\" aria-hidden=\"true\" style=\"padding-right: 10px;\"></span>" + value.name + "</a>"
 	});
 	return result;
@@ -258,109 +375,6 @@ function buildResultScoresHtml() {
 	return result;
 }
 
-function getGameData(gameNo, gameSource, failReason) {
-	var url = "";
-	var result = "";
-
-	url = game.games[gameNo - 1].data;
-	
-	if (url.length > 0) {
-		alertify.success("pulling game from internet...please wait...");
-		$.ajax({
-			type: 'GET',
-		    url: url,
-		    timeout: 10000,
-		    success: function( response ) {
-		    	gameSource(true, response);
-		        //result = response; //jQuery.parseJSON( response );;
-		    },
-		    error: function(xhr, textStatus, errorThrown){
-		       gameSource(false, undefined, textStatus + " - " + errorThrown);
-		    }
-		});
-	} else {
-		gameSource(false, undefined, "Invalid game option selected");
-	}
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-function getGameSetup(loadGame) {
-	var url = "https://api.myjson.com/bins/lj2g";
-
-	$.ajax({
-		type: 'GET',
-	    url: url,
-	    timeout: 10000,
-	    success: function( response ) {
-			loadGame(response);
-	    },
-	    error: function(xhr, textStatus, errorThrown){
-	       alertify.error("Could not get game data");
-	    }
-	});
-}
-
-function configure() {
-	getGameSetup(function(gameSetup) {
-		game = gameSetup;
-		resetGame();
-	});
-}
-
-
-var game;
-var gameData;
-var newGameSource;
-
-
-$(window).on("load", function() {
-	configure();
-});
-
-// function getGiphy(keyword) {
-// 	//var url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + encodeURIComponent(keyword);
-    
-// 	$.ajax({
-// 		type: 'GET',
-// 	    url: url,
-// 	    dataType: "jsonp",
-// 	    timeout: 5000,
-// 	    success: function( response ) {
-// 	        console.log( response ); // server response
-// 	        var imageSource = response.data.image_original_url;
-// 	        if (imageSource == undefined)
-// 	        	$("#gameGiphy").attr("src","https://media.giphy.com/media/u41lLJnRAzRi8/giphy.gif");
-// 	        else
-// 	    		$("#gameGiphy").attr("src",imageSource);
-// 	    },
-// 	    error: function(xhr, textStatus, errorThrown){
-// 	       $("#gameGiphy").attr("src","https://media.giphy.com/media/u41lLJnRAzRi8/giphy.gif");
-// 	    }
-// 	});
-// }
-
-
-
-	// gameData.source = {
-	// 	version : "1.0",
-	// 	gameVersion : "1.0",
-	// 	name : "isiZulu to English",
-	// 	words : [
-	// 		{
-	// 			word : "Isikhombisa",
-	// 			answers : [ "Seven", "7" ],
-	// 			score : 0,
-	// 			given : ""
-	// 		},
-	// 		{
-	// 			word : "Kuyabanda",
-	// 			answers : [ "It is hot", "hot", "it's hot", "its hot" ],
-	// 			score : 0,
-	// 			given : ""
-	// 		}
-	// 	]
-	// }
-
-
-// $(document).on("keypress", "form", function(event) { 
-//     return event.keyCode != 13;
-// });
